cb30a349441394c239d6e940fbe181f38d3ebf17
Guilherme Fonseca <guilhermesfonsecaa@gmail.com>
2021-12-14 01:03:32 -0300

    toma lucao

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,11 @@
 $(document).ready(function(){
+    // Slider Home
+    $('.main-slider').slick({
+        arrows: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+    })
+    
     function menu () {
         $('.header__menu').on('click', function () {
             $('.nav').toggleClass('nav__mobile');
