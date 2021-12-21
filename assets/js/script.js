40b458ff75c70ef00eb57adf03cb4e09734d67fd
Guilherme Fonseca <guilhermesfonsecaa@gmail.com>
2021-12-21 19:18:01 -0300

    galeria

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,6 +21,35 @@ $(document).ready(function(){
             }
           ]
     })
+
+    // Galeria modal
+
+    function modal () {
+        $('.modal-slider').slick({
+            infinite: false,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+        });
+    
+        $('.modal-close').on('click', function () {
+            $('.modal').removeClass('modal--active');
+        });
+    
+        $('.galeria-card-link').on('click', function () {
+            $(this).parent().siblings('.modal').addClass('modal--active');
+            $('.modal-slider').slick('refresh');
+    
+            let ultimoItem = $('.modal--active .modal-slider img').last().data('item');
+            
+            $('.modal--active .modal-paginacao-item').text('/0' + ultimoItem);
+        });
+    
+        $(".modal-slider").on("afterChange", function () {
+            let ativoItem = $('.modal--active .modal-slider .slick-current').data('item');
+            $('.modal--active .modal-paginacao-item--active').text('0'+ativoItem);
+        });
+    }
+    modal();
     
     function menu () {
         $('.header__menu').on('click', function () {
